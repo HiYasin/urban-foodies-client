e045c21000b5f4b26142408e0031c613d05b1431
HiYasin <hello.mr.faisal@gmail.com>
2025-02-12 19:06:17 +0600

    Eye catching error page added

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
-import register from '../assets/register.json';
+import error from '../assets/error.json';
 import Lottie from 'lottie-react';
 const PageNotFound = () => {
     return (
         <div className='w-full h-[100vh] flex flex-col justify-center items-center gap-5'>
-            <h5 className='text-5xl md:text-8xl'>404</h5>
-            <Lottie animationData={register}></Lottie>
-            <h5 className='text-2xl md:text-4xl'>Page Not Found!</h5>
+            <Lottie animationData={error}></Lottie>
+            <h5 className='text-2xl md:text-4xl text-orange-600'>Page Not Found!</h5>
             <Link to={'/'}>
-                <button className='btn btn-outline'>Go to Home</button>
+                <button className='btn btn-outline border-orange-600 text-orange-600 hover:text-orange-600 hover:bg-orange-600/30 hover:border-orange-600'>Go to Home</button>
             </Link>
         </div>
     );
